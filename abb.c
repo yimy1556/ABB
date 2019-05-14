08db45bc4d9829f883cc1e0ecd146e3ea52ab82d
yimy1556 <llanosyimy@gmail.com>
2019-05-14 18:20:08 -0300

    se añadio busqueda_ptr

--- abb.c
+++ abb.c
@@ -1,8 +1,9 @@
 #include "abb.h"
 #include <stdlib.h>
+#include <string.h>
 #define INICIO 0
 
-struct nodo{
+typedef struct nodo{
     struct nodo* izq;
     struct nodo* der;
     char* clave;
@@ -16,13 +17,40 @@ struct abb {
     size_t cantidad;
 };
 
-abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
-    abb_t* abb = malloc(sizeof(abb_t));
-    if(!abb) return NULL;
-    abb->raiz = NULL;
-    abb->destruir_dato = destruir_dato;
-    abb->comparar_clave = cmp;    
-    abb->cantidad = INICIO;
-    return abb;
+nodo_t* nodo_crear(const char *clave, void *dato){
+    nodo_t* nodo = malloc(sizeof(nodo_t));
+    if(!nodo) return NULL;
+    nodo->izq = NULL;
+    nodo->der = NULL;
+    strcpy(nodo->clave,clave);
+    nodo->dato = dato;
+    return nodo;
 }
 
+/*bool agregar_hoja(nodo_t* nodo ,nodo_t* nuevo_nodo ,int* cantidad){
+    if(!nuevo_nodo) return false;
+    nodo = nuevo_nodo
+    *cantidad++;
+    return true;
+}*/
+
+abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
+    abb_t* arbol = malloc(sizeof(abb_t));
+    if(!arbol) return NULL;
+    arbol->raiz = NULL;
+    arbol->destruir_dato = destruir_dato;
+    arbol->comparar_clave = cmp;    
+    arbol->cantidad = INICIO;
+    return arbol;
+}
+
+
+nodo_t** busqueda_ptr(nodo_t** ptr_nodo,abb_comparar_clave_t comparar,char* clave){
+    if (!(*ptr_nodo)) return ptr_nodo;
+    
+    int comparacion = comparar((*ptr_nodo)->clave,clave); 
+    if (!comparacion) return ptr_nodo;    
+    
+    if (comparacion < 0) return busqueda_ptr(&((*ptr_nodo)->izq),comparar,clave);
+    else return busqueda_ptr(&((*ptr_nodo)->der),comparar,clave);
+}
